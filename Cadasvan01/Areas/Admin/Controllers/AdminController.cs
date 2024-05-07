21b8a36814ec016728a55e76861b67770e4f8b6d
VinnyPA <vinny.alves004@hotmail.com>
2024-05-07 20:16:56 -0300

    Update AdminController.cs

--- Cadasvan01/Areas/Admin/Controllers/AdminController.cs
+++ Cadasvan01/Areas/Admin/Controllers/AdminController.cs
@@ -5,10 +5,12 @@ using Cadasvan01.Services;
 using Cadasvan01.Data;
 using Microsoft.EntityFrameworkCore;
 using Cadasvan01.Enums;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Cadasvan01.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
         public IActionResult Index()
