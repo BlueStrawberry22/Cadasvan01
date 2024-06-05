64f7f735b335a712eaac42056cd5cf050d60fd07
VinnyPA <vinny.alves004@hotmail.com>
2024-06-05 00:29:11 -0300

    controler no motorista

--- Cadasvan01/Areas/Motorista/Controllers/MotoristaPresencasController.cs
+++ Cadasvan01/Areas/Motorista/Controllers/MotoristaPresencasController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Cadasvan01.Data;
 using Cadasvan01.Models;
-using Microsoft.AspNetCore.Identity;
 
 namespace Cadasvan01.Areas.Motorista.Controllers
 {
@@ -15,37 +14,21 @@ namespace Cadasvan01.Areas.Motorista.Controllers
     public class MotoristaPresencasController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private readonly UserManager<Usuario> _userManager;
 
-        public MotoristaPresencasController(ApplicationDbContext context, UserManager<Usuario> userManager)
+        public MotoristaPresencasController(ApplicationDbContext context)
         {
             _context = context;
-            _userManager = userManager;
         }
 
         // GET: Motorista/MotoristaPresencas
         public async Task<IActionResult> Index()
         {
-
-            var motoristaID = _userManager.GetUserId(User);
-            var motorista = await _context.Usuarios.FindAsync(motoristaID);
-            if (motorista == null || motorista.MotoristaId == null)
-            {
-                return NotFound();
-            }
-
-            var presencas = await _context.Presencas
-                .Where(a => a.UsuarioId == motorista.MotoristaId)
-                .ToListAsync();
-
             var applicationDbContext = _context.Presencas.Include(p => p.Motorista).Include(p => p.Usuario);
             return View(await applicationDbContext.ToListAsync());
         }
 
-        // está comentado pois primerio precisamos ver se está funcionando o jeito casual 
-
-
-     /*   public async Task<IActionResult> Details(int? id)
+        // GET: Motorista/MotoristaPresencas/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
@@ -62,7 +45,7 @@ namespace Cadasvan01.Areas.Motorista.Controllers
             }
 
             return View(presenca);
-        }*/
+        }
 
     }
 }
