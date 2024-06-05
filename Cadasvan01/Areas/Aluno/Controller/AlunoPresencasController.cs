8c57e79345ecb9537ec9d23957b60d880970270c
VinnyPA <vinny.alves004@hotmail.com>
2024-06-05 02:18:47 -0300

    está dando certo

--- Cadasvan01/Areas/Aluno/Controller/AlunoPresencasController.cs
+++ Cadasvan01/Areas/Aluno/Controller/AlunoPresencasController.cs
@@ -27,12 +27,16 @@ namespace Cadasvan01.Areas.Aluno.Controllers
         // GET: Aluno/AlunoPresencas
         public async Task<IActionResult> Index()
         {
-            var motoristaID = "motoristaID";
-            var usuariosConfirmados = await _context.Presencas.Where(w => w.DataViagem == DateTime.Now)
+            var alunoId = _userManager.GetUserId(User);
+
+            // Filtrar as presenças apenas para o aluno atual
+            var presencasAluno = await _context.Presencas
+                .Include(p => p.Motorista)
+                .Include(p => p.Usuario)
+                .Where(p => p.UsuarioId == alunoId) // Filtrar pelo ID do aluno
                 .ToListAsync();
 
-            var applicationDbContext = _context.Presencas.Include(p => p.Motorista).Include(p => p.Usuario);
-            return View(await applicationDbContext.ToListAsync());
+            return View(presencasAluno);
         }
 
         // GET: Aluno/AlunoPresencas/Details/5
@@ -58,7 +62,15 @@ namespace Cadasvan01.Areas.Aluno.Controllers
         // GET: Aluno/AlunoPresencas/Create
         public IActionResult Create()
         {
-            
+            // Obter o ID do aluno atualmente logado
+            var alunoId = _userManager.GetUserId(User);
+
+            // Definir o UsuarioId na ViewData para preencher o campo oculto no formulário
+            ViewData["UsuarioId"] = alunoId;
+
+            // Carregar a lista de motoristas disponíveis
+            ViewData["MotoristaId"] = new SelectList(_context.Usuarios.Where(w => w.Tipo == Enums.UsuarioEnum.Motorista), "Id", "NomeCompleto");
+
             return View();
         }
 
@@ -67,16 +79,25 @@ namespace Cadasvan01.Areas.Aluno.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("PresencaId,UsuarioId,MotoristaId,DataViagem,ConfirmaIda,ConfirmaVolta")] Presenca presenca)
+        public async Task<IActionResult> Create(Presenca presenca)
         {
+            // Obter o ID do aluno atualmente logado
+            var alunoId = _userManager.GetUserId(User);
+
+            // Definir o UsuarioId na presenca com o ID do aluno logado
+            presenca.UsuarioId = alunoId;
+
             if (ModelState.IsValid)
             {
                 _context.Add(presenca);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["MotoristaId"] = new SelectList(_context.Usuarios, "Id", "NomeCompleto", presenca.MotoristaId);
-            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "NomeCompleto", presenca.UsuarioId);
+            // Carregar a lista de motoristas disponíveis
+            ViewData["MotoristaId"] = new SelectList(_context.Usuarios.Where(w => w.Tipo == Enums.UsuarioEnum.Motorista), "Id", "NomeCompleto", presenca.MotoristaId);
+
+            // Como o UsuarioId é definido no método GET, não é necessário adicioná-lo aqui
+
             return View(presenca);
         }
 
@@ -93,10 +114,25 @@ namespace Cadasvan01.Areas.Aluno.Controllers
             {
                 return NotFound();
             }
-            ViewData["MotoristaId"] = new SelectList(_context.Usuarios.Where(w => w.Tipo == Enums.UsuarioEnum.Motorista), "Id", "NomeCompleto"); ;
-            ViewData["UsuarioId"] = new SelectList(_context.Usuarios.Where(w => w.Tipo == Enums.UsuarioEnum.Aluno), "Id", "NomeCompleto");
+
+            // Obter o ID do aluno atualmente logado
+            var alunoId = _userManager.GetUserId(User);
+
+            // Verificar se o aluno logado é o dono da presença
+            if (presenca.UsuarioId != alunoId)
+            {
+                return Unauthorized(); // O aluno não tem permissão para editar esta presença
+            }
+
+            // Preencher ViewData apenas para motoristas vinculados ao aluno
+            ViewData["MotoristaId"] = new SelectList(_context.Usuarios.Where(w => w.Tipo == Enums.UsuarioEnum.Motorista), "Id", "NomeCompleto", presenca.MotoristaId);
+
+            // Definir o UsuarioId na ViewData para preencher o campo oculto no formulário
+            ViewData["UsuarioId"] = alunoId;
+
             return View(presenca);
         }
+
 
         // POST: Aluno/AlunoPresencas/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
@@ -130,8 +166,10 @@ namespace Cadasvan01.Areas.Aluno.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["MotoristaId"] = new SelectList(_context.Usuarios, "Id", "NomeCompleto", presenca.MotoristaId);
-            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "NomeCompleto", presenca.UsuarioId);
+            ViewData["MotoristaId"] = new SelectList(_context.Usuarios.Where(w => w.Tipo == Enums.UsuarioEnum.Motorista), "Id", "NomeCompleto", presenca.MotoristaId);
+
+            // Como o UsuarioId é definido no método GET, não é necessário adicioná-lo aqui
+
             return View(presenca);
         }
 
